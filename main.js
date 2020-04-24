3c8a555a4b1f711ee2be230d04926a1bcebad135
colorlessenergy <brianmunozdev@gmail.com>
2020-04-23 18:36:18 -0700

    add mobile support #11

--- main.js
+++ main.js
@@ -16,7 +16,20 @@
 
 
   canvas.addEventListener('mousemove', paintCanvas);
-  canvas.addEventListener('touchmove', paintCanvas);
+
+  canvas.addEventListener("touchmove", function (ev) {
+    // prevent scrolling
+    if (ev.target == canvas) {
+      ev.preventDefault();
+    }
+
+    var touch = ev.touches[0];
+    var mouseEvent = new MouseEvent("mousemove", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
 
 
   socket.on('transmit pixel data', function (data) {
@@ -24,20 +37,37 @@
     colorCanvas(data);
   });
 
+  /**
+   * 
+   * @param {Object} data - holds the position X, Y and color for drawing on the canvas
+   * @param {Number} data.x - the x position of the input
+   * @param {Number} data.Y - the Y position of the input
+   * 
+   * 
+   * takes in data to draw on the canvas
+   */
+
   function colorCanvas (data) {
     ctx.fillStyle = data.color;
     ctx.fillRect(data.x, data.y, PIXEL_SIZE, PIXEL_SIZE);
   }
 
   function paintCanvas(ev) {
-    let pixelData = {
-      x: ev.offsetX - 1,
-      y: ev.offsetY - 1,
-      color: window.app.color
-    }
-
+    let pixelData = getMousePos(canvas, ev);
+    pixelData.color = window.app.color;
+    
     colorCanvas(pixelData);
     socketController.sendPixelData(pixelData)
+  }
+
+  // Get the position of the mouse relative to the canvas
+  function getMousePos(canvasDom, mouseEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+      x: mouseEvent.clientX - rect.left,
+      y: mouseEvent.clientY - rect.top
+    };
+
   }
 
 })();
